358cbed9620fdafb934f116f491217b58200e06d
jaydenThomson1999 <jaydenthomson1999@gmail.com>
2019-07-25 15:43:42 +1000

    add implementation for login attempt

--- week4/src/app/login/login.component.ts
+++ week4/src/app/login/login.component.ts
@@ -1,17 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 
+const users = {
+  'johndoe@domain.com': 'password',
+  'janedoe@domain.com': 'qwerty',
+  'danejohns@domain.com': 'asdf'
+};
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  email = '';
-  password = '';
+  private email = '';
+  private password = '';
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  login_attempt() {
+    console.log(this.email, this.password);
+    let ok = false;
+
+    if (users.hasOwnProperty(this.email)) {
+      if (this.password === users[this.email]) {
+          ok = true;
+      }
+    }
+
+    console.log(ok);
+  }
 }
