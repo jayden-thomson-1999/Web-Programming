663094f7ea9160a65ee362951e64f3e8b3e3edee
Jayden Thomson <jaydenthomson1999@gmail.com>
2019-08-18 22:04:17 +1000

    changed chat ui to have message input on top

--- week6/src/app/chat-room/chat-room.component.ts
+++ week6/src/app/chat-room/chat-room.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat-room.component.css']
 })
 export class ChatRoomComponent implements OnInit {
-  private messages;
+  private messages: string[];
+  private messageIn: string;
 
   constructor() {
     this.messages = [
