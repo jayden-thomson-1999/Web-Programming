df5bbda41874f74956f14bf51c585cb693add99b
Jayden Thomson <jaydenthomson1999@gmail.com>
2019-08-18 20:34:38 +1000

    add link to chat roomn

--- week6/src/app/app-routing.module.ts
+++ week6/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ChatRoomComponent } from './chat-room/chat-room.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'chat-room', component: ChatRoomComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
