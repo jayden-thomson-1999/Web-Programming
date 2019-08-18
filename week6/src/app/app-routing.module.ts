0d08edd8964e70498158f0f86115aa9c192060b7
Jayden Thomson <jaydenthomson1999@gmail.com>
2019-08-18 21:05:31 +1000

    add message-input interface

--- week6/src/app/app-routing.module.ts
+++ week6/src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 import { ChatRoomComponent } from './chat-room/chat-room.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'chat-room', component: ChatRoomComponent }
+  { path: '', component: ChatRoomComponent }
 ];
 
 @NgModule({
--- week6/src/app/app.module.ts
+++ week6/src/app/app.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 import { ChatRoomComponent } from './chat-room/chat-room.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
     ChatRoomComponent
   ],
   imports: [
