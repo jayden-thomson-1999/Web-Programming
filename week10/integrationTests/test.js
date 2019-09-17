c76a6abe596f6f04d319ba3d2253040c1e36ce12
Jayden Thomson <jaydenthomson1999@gmail.com>
2019-09-17 16:55:58 +1000

    add integration test.js file skeleton, need implementation

---
+++ week10/integrationTests/test.js
@@ -0,0 +1,33 @@
+const assert = require('assert');
+const app = require('../server/server');
+const http = require('http');
+
+describe('Route Test', () => {
+    describe('api-add.js', ()=>{
+
+    })
+
+    describe('api-deleteitem.js', ()=>{
+
+    })
+
+    describe('api-getitem.js', ()=>{
+
+    })
+
+    describe('api-getlist.js', ()=>{
+
+    })
+
+    describe('api-prodcount.js', ()=>{
+
+    })
+
+    describe('api-update.js', ()=>{
+
+    })
+
+    describe('api-validid.js', ()=>{
+
+    })
+});
