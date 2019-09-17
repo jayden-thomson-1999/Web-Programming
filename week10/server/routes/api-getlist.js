9a55eaa3bb4af660ad96f82055b3d547f316004b
Jayden Thomson <jaydenthomson1999@gmail.com>
2019-09-17 22:12:53 +1000

    got rid of part b

--- week10/server/routes/api-getlist.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(db, app) {
-    app.get('/api/getlist', function(req, res) {
-        const collection = db.collection('products');
-        collection.find({}).toArray((err, data) => {
-            res.send(data);
-        });
-    });
-}
